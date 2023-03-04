62cc2b6846425ecd61f025ae30c8af203f4cb87c
fendie07 <104850385+fendie07@users.noreply.github.com>
2023-03-03 21:33:21 -0500

    created login and register page and updated nav

---
+++ Scripts/login.js
@@ -0,0 +1,6 @@
+let mainContent = document.getElementById("main")
+mainContent.setAttribute("class", "container")
+
+let title = document.getElementById("title")
+
+        title.textContent = "Login"
---
+++ Scripts/register.js
@@ -0,0 +1,11 @@
+let mainContent = document.getElementById("main")
+mainContent.setAttribute("class", "container")
+
+let title = document.getElementById("title")
+
+title.textContent = "Register"
+let mainParagraph = document.createElement("p")
+        mainParagraph.setAttribute("id", "MainParagraph")
+        mainParagraph.setAttribute("class", "mt-3")
+
+        mainParagraph.textContent = "Create your own account. It's free and only takes a minute! We only record 50% of your private data including SIN and Bank Info!"
